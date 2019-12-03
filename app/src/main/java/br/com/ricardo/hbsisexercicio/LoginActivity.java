56008373f0841997d3f952bfd4f3d5feb5a2e01c
Ricardo Gonçalves <ricardo@brgweb.com.br>
2019-12-03 11:23:25 -0300

    StartActivityForResult e SharedPreferences
    Exemplo de como usar StartActivityForResult e SharedPreferences

---
+++ app/src/main/java/br/com/ricardo/hbsisexercicio/LoginActivity.java
@@ -0,0 +1,33 @@
+package br.com.ricardo.hbsisexercicio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acitivity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent();
+                data.putExtra("uid",456);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+    }
+
+}
--- app/src/main/java/br/com/ricardo/hbsisexercicio/MainActivity.java
+++ app/src/main/java/br/com/ricardo/hbsisexercicio/MainActivity.java
@@ -1,17 +1,50 @@
 package br.com.ricardo.hbsisexercicio;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int LOGIN_REQUEST_CODE = 111;
+    SharedPreferences prefs;
+    private int uid = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        prefs = getSharedPreferences("HBSISEx", MODE_PRIVATE);
+        uid = prefs.getInt("uid", 0);
+        if(uid == 0){
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivityForResult(loginIntent, LOGIN_REQUEST_CODE);
+        }else{
+            setLayout();
+        }
+    }
 
-        Log.i("AVISO", "BANCH RICARDO");
+    private void setLayout() {
+        setContentView(R.layout.activity_main);
+        TextView user = findViewById(R.id.usuario);
+        user.setText("User ID: "+String.valueOf(uid));
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOGIN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                uid = data.getIntExtra("uid", 0);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putInt("uid",uid);
+                editor.apply();
+                setLayout();
+            }else{
+                Toast.makeText(this,"Falha no Login", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
